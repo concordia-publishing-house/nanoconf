a2ad6e3efd8f7f36188af173f8aa4995587b152f
Bob Lail <bob.lail@cph.org>
2013-05-16 16:07:06 -0500

    [skip] Generated empty Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nanoconf::Application.config.secret_token = '399a1edb5daca6f631247a2fef3d87b79b316162020bf4221050f1cdb4479d3ab8636414140e8d7780bb738442b25db9c8d6a8f400d8dc7d2e93cec53ebc8f22'
